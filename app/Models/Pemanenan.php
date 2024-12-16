9234c56db06126d08680b0ab5e7bbf8ced524640
sheva-123 <shevarmdhn041006@gmail.com>
2024-12-16 11:09:17 +0700

    p

--- app/Models/Pemanenan.php
+++ app/Models/Pemanenan.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 
 class Pemanenan extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['id_penanaman', 'tanggal_pemanenan', 'jumlah_hasil'];
+
+    public function planting()
+    {
+        return $this->belongsTo(Penanaman::class, 'id_penanaman');
+    }
 }
--- app/Models/Pengeluaran.php
+++ app/Models/Pengeluaran.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pengeluaran extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['id_pertanian', 'tanggal_pengeluaran', 'jenis_pengeluaran', 'biaya'];
+
+    public function farm()
+    {
+        return $this->belongsTo(Pertanian::class, 'id_pertanian');
+    }
 }
+
