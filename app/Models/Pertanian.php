72604e5fd543b2b7371688527432da0a8e11c8b3
Alwi376 <alwiisihab394@gmail.com>
2024-12-16 11:19:26 +0700

    q

--- app/Models/Pertanian.php
+++ app/Models/Pertanian.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pertanian extends Model
 {
-    //
+    protected $fillable = [
+        'nama_pertanian',
+        'lokasi_pertanian',
+        'luas_lahan',
+    ];
+
+    public function penanaman(){
+        return $this->hasMany(Penanaman::class);
+    }
 }
