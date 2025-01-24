383894325885d2cfa6abba3cca535fb2190bb25b
unknown <sandyriswantino1933@gmail.com>
2025-01-24 09:27:58 +0700

    anjeng

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@ use App\Http\Controllers\PemanenanController;
 use App\Http\Controllers\PengeluaranController;
 use App\Http\Controllers\LaporanController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminDashboardController;
+use App\Http\Controllers\UserHomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -25,24 +28,16 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('admin.dashboard');
-    })->name('admin.dashboard');
+    Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
 
-<<<<<<< HEAD
-    Route::resource('/pertanians', PertanianController::class);
-=======
     Route::resource('pertanians', PertanianController::class);
     Route::resource('pengguna', UserController::class)->names('pengguna');
 
     Route::get('/assign-land', [UserController::class, 'tambahlahan'])->name('admin.tambahlahanpetani');
->>>>>>> 0a1db172825a36ae65e553b84e4c267e89e81ec8
 });
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/home', function () {
-        return view('petani.dashboard');
-    })->name('user.home');
+    Route::get('/home', [UserHomeController::class, 'index'])->name('user.home');
     Route::resource('tanamans', TanamanController::class);
     Route::resource('Penanamans', PenanamanController::class);
     Route::resource('pemeliharaans', PemeliharaanController::class);
