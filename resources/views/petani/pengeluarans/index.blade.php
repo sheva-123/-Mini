e471cddca58b92b677f0881d2f93822eeb655041
Alwi376 <alwiisihab394@gmail.com>
2025-02-18 23:41:24 +0700

    rvs

--- resources/views/petani/pengeluarans/index.blade.php
+++ resources/views/petani/pengeluarans/index.blade.php
@@ -62,7 +62,7 @@
                                 <td class="px-6 py-4 flex items-center space-x-4">
                                     <a href="{{ route('pengeluarans.edit', $pengeluaran) }}"
                                         class="text-yellow-500 hover:text-yellow-700">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none"
                                             viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                 d="M15.232 4.232l4.536 4.536-9 9H6v-4.768l9-9zM9 11l3 3" />
